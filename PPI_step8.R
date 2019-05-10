31898c6453e86e513b8b6ac884d94a6eb3e07dc2
nmuncy <pingamormn@gmail.com>
2019-05-10 14:27:03 -0600

    update stats script

--- PPI_step8.R
+++ PPI_step8.R
@@ -1,99 +1,35 @@
+
 library(tiff)
 library(reshape2)
 library(ez)
 
 
 
-### --- Notes
-#
 
 
-
-
-###################
-# Set up
-###################
+### Set up
+doWrite <- 1
 
 parDir <- "/Volumes/Yorick/Nate_work/AutismOlfactory/Analyses/"
-
-doWrite <- 1
-doGraphs <- 1
-
-etacDir <- paste0(parDir,"grpAnalysis/")
-etacData_location <- paste0(etacDir,"MVM_betas/")
-etacData_list <- read.table(paste0(etacData_location,"All_List.txt"))
-etac_outDir <- etacData_location
-etac_statsDir <- paste0(etacDir,"MVM_stats/")
+aoDir <- paste0(parDir,"grpAnalysis/")
+aoData_location <- paste0(aoDir,"MVM_betas/")
+aoData_list <- read.table(paste0(aoData_location,"All_List.txt"))
+ao_statsDir <- paste0(aoDir,"MVM_stats/")
 
 
 
-
-###################
-# Functions
-###################
-
-SE.Function <- function(x,plot_data){
-  SD <- sd(plot_data[,x])/sqrt(length(plot_data[,x]))
-  return(SD)
-}
-
-# Graph.Function <- function(DF,output_name,maskN,out_place){
-#   
-#   TITLE <- maskN
-#   MEANS <- colMeans(DF)
-#   E.BARS<-NA
-#   for(a in 1:dim(DF)[2]){
-#     E.BARS[a] <- SE.Function(a,DF)
-#   }
-#   RANGE <- range(c(MEANS,MEANS-E.BARS,MEANS+E.BARS,0))
-#   
-#   if(grepl("ns_stats",out_place)==T){
-#     ROI <- NsNames.Function(maskN)
-#     XNAMES <- GraphNames.Function(output_name)
-#   }else if(grepl("etac_stats",out_place)==T){
-#     ROI <- EtacNames.Function(output_name,maskN)
-#     XNAMES <- GraphEtacNames.Function(output_name)
-#   }else if(grepl("sub_stats",out_place)==T){
-#     ROI <- maskN
-#     XNAMES <- GraphNames.Function(output_name)
-#   }
-#   
-#   plotable <- matrix(0,nrow=2,ncol=num.betas)
-#   plotable[1,] <- MEANS
-#   plotable[2,] <- E.BARS
-#   
-#   if(doWrite == 1){
-#     graphOut <- paste0(out_place,"Graph_",output_name,"_",TITLE,".tiff")
-#     bitmap(graphOut, width = 6.5, units = 'in', type="tiff24nc", res=1200)
-#   }
-#   barCenters <- barplot(plotable[1,], names.arg = c(XNAMES), main=ROI, ylab="Beta Coefficient",ylim=RANGE)
-#   segments(barCenters, MEANS-E.BARS, barCenters, MEANS+E.BARS)
-#   arrows(barCenters, MEANS-E.BARS, barCenters, MEANS+E.BARS, lwd = 1, angle = 90, code = 3, length = 0.05)
-#   set.pos <- rowMeans(plotable); if(set.pos[1]>0){POS<-3}else{POS<-1}
-#   text(barCenters,0,round(plotable[1,],4),cex=1,pos=POS,font=2)
-#   if(doWrite == 1){
-#     dev.off()
-#   }
-# }
-
-
-
-
-###################
-# ETAC
-###################
-# # For testing
+# For testing
 # i <- "All_Betas_FUMC_2.txt"
 # j <- "Betas_FUMC_2_LINS.txt"
 
-for(i in t(etacData_list)){
+for(i in t(aoData_list)){
   
-  beta_list <- read.table(paste0(etacData_location,i))
+  beta_list <- read.table(paste0(aoData_location,i))
 
   for(j in t(beta_list)){
     
     ### Get, clean data
-    raw_data <- read.delim2(paste0(etacData_location,j),header=F)
+    raw_data <- read.delim2(paste0(aoData_location,j),header=F)
     
     # num subjects
     ind.subj <- grep("File", raw_data[,1])
@@ -132,7 +68,49 @@ for(i in t(etacData_list)){
     
     output <- c("Con Mean", con_mean, "Aut Mean", aut_mean)
     output <- c(output, capture.output(print(hold_out)))
-    writeLines(output,paste0(etac_statsDir, "Stats_", j))
+    if(doWrite == 1){
+      writeLines(output,paste0(ao_statsDir, "Stats_", j))
+    }
+    
+    
+    ### Graph
+    plotable <- matrix(0,nrow=2,ncol=2)
+    colnames(plotable) <- c("Control","Autistic")
+    plotable[1,] <- c(con_mean,aut_mean)
+    
+    con_sd <- sd(df[ind.con,2])/sqrt(length(ind.con))
+    aut_sd <- sd(df[ind.aut,2])/sqrt(length(ind.aut))
+    plotable[2,] <- c(con_sd,aut_sd)
+    
+    if(doWrite == 1){
+      tmp <- gsub("\\..*","",j)
+      graphOut <- paste0(ao_statsDir, "Graph_", tmp, ".tiff")
+      bitmap(graphOut, width = 6.5, units = 'in', type="tiff24nc", res=1200)
+    }
+
+    hold <- gsub("^.*?_","",j); decon <- gsub("_.*$","",hold)
+    hold2 <- gsub("^.*?_","",hold); seed <- gsub("_.*$","",hold2)
+    hold3 <- gsub("^.*?_","",hold2); clust <- gsub("\\..*","",hold3)
+    
+    # hardcode stim since so few
+    if(decon == "FUMvC"){
+      stim <- "Odor"
+    }else{
+      stim <- "UBO"
+    }
+    
+    MEANS <- plotable[1,]
+    E.BARS <- plotable[2,]
+    RANGE <- range(c(MEANS,MEANS-E.BARS,MEANS+E.BARS,0))
+    
+    MAIN <- paste0(stim, ": Seed ", seed, "-", clust)
+    
+    barCenters <- barplot(plotable[1,], main=MAIN, ylab="Parameter Estimate", ylim=RANGE)
+    segments(barCenters, MEANS-E.BARS, barCenters, MEANS+E.BARS)
+    arrows(barCenters, MEANS-E.BARS, barCenters, MEANS+E.BARS, lwd = 1, angle = 90, code = 3, length = 0.05)
+    
+    if(doWrite == 1){
+      dev.off()
+    }
   }
 }
-
