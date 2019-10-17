2c4ab1094bd0c5b695b4eeffff2487a5c4472a0a
nmuncy <pingamormn@gmail.com>
2019-10-17 09:54:53 -0600

    just mask/noise modelling. working

--- Task_step4_grpAnalysis.sh
+++ Task_step4_grpAnalysis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --time=40:00:00   # walltime
-#SBATCH --ntasks=10   # number of processor cores (i.e. tasks)
+#SBATCH --time=05:00:00   # walltime
+#SBATCH --ntasks=6   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=10gb   # memory per CPU core
+#SBATCH --mem-per-cpu=8gb   # memory per CPU core
 #SBATCH -J "TS4"   # job name
 
 # Compatibility variables for PBS. Delete if not needed.
@@ -87,50 +87,22 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 parDir=~/compute/AutismOlfactory
 workDir=${parDir}/derivatives								# par dir of data
 outDir=${parDir}/Analyses/grpAnalysis						# where output will be written (should match step3)
-stimDir=${parDir}/stimuli
 refFile=${workDir}/sub-1048/run-3_AO_scale+tlrc				# reference file, for finding dimensions etc
 
 tempDir=${parDir}/Template									# desired template
 priorDir=${tempDir}/priors_ACT								# location of atropos priors
 mask=Intersection_GM_mask+tlrc								# this will be made, just specify name for the interesection gray matter mask
 
-
-# grpAnalysis
-runIt=0														# whether ETAC/MVM scripts actually run (and not just written) (1)
 thr=0.3														# thresh value for Group_EPI_mask, ref Group_EPI_mean
+blurM=2														# blur multiplier, float/int
 
 compList=(FUMC OC SMC)										# matches decon prefixes, and will be prefix of output files
 compLen=${#compList[@]}
-
-arrA=(1 3 5 7)												# setA beh sub-brik for compList. Must be same length as compList
-arrB=(1 3)													# setB
-arrC=(1 3 5)
-wsArr=ABC													# list of within-subject arrays used (arrA, arrB, etc), for building permutations (e.g. wsArr=ABC)
-
-namA=(Mask FBO UBO CA)										# names of behaviors from arrA. Must be same length as arrA
-namB=(CA Odor)
-namC=(CA Mask FUBO)
-
-
-# MVM vars/arrs
-blurM=2														# blur multiplier, float/int
-bsArr=(OCD Con)												# Bx-subject variables (groups)
-# bsList=${outDir}/Group_list.txt								# Needed when bsArr > 1. List where col1 = subj identifier, col2 = group membership (e.g. s1295 Con)
-
-
-# covariates and group membership
-covSubj=(`tail -n +2 ${stimDir}/Cov_list.txt | awk '{print $1}'`)
-covGroup=(`tail -n +2 ${stimDir}/Cov_list.txt | awk '{print $2}'`)
-covSnif=(`tail -n +2 ${stimDir}/Cov_list.txt | awk '{print $3}'`)
-covSPA=(`tail -n +2 ${stimDir}/Cov_list.txt | awk '{print $4}'`)
-covIUS=(`tail -n +2 ${stimDir}/Cov_list.txt | awk '{print $5}'`)
-covRBQ=(`tail -n +2 ${stimDir}/Cov_list.txt | awk '{print $6}'`)
 
 
 
 
 ### --- Functions --- ###
-
 
 # search array for string
 MatchString () {
@@ -143,41 +115,6 @@ MatchString () {
 	done
 	return 1
 }
-
-
-# make perumtations of length 2
-MakePerm () {
-
-	local items=$1
-	local string i j hArr
-
-	for ((i=0; i<${#items}; i++)); do
-
-		string=${items:$i+1}
-		for ((j=0; j<${#string}; j++)); do
-
-			hArr+="${items:$i:1}${string:$j:1} "
-		done
-	done
-	echo $hArr
-}
-
-
-
-
-### --- Generate --- ###
-
-
-# make permutation lists
-arr=(`MakePerm $wsArr`)
-alpha=(`echo {A..Z}`)
-wsList=(${alpha[@]:0:${#wsArr}})
-
-
-for ((a=0; a<${#bsArr[@]}; a++)); do
-	tmpList+=$a
-done
-arrBS=(`MakePerm $tmpList`)
 
 
 
@@ -193,87 +130,73 @@ arrBS=(`MakePerm $tmpList`)
 
 cd $outDir
 
-if [ $runIt == 1 ]; then
+# intersection mask
+if [ ! -f Group_epi_mask.nii.gz ]; then
 
-	# intersection mask
-	if [ ! -f Group_epi_mask.nii.gz ] && [ ! -f etac_extra/Group_epi_mask.nii.gz ]; then
+	for i in ${workDir}/s*; do
+		subj=${i##*\/}
+		MatchString "$subj" "${arrRem[@]}"
+		if [ $? == 1 ]; then
+			list+="${i}/mask_epi_anat+tlrc "
+		fi
+	done
 
-		for i in ${workDir}/s*; do
-
-			subj=${i##*\/}
-			MatchString "$subj" "${arrRem[@]}"
-
-			if [ $? == 1 ]; then
-				list+="${i}/mask_epi_anat+tlrc "
-			fi
-		done
-
-		3dMean -prefix ${outDir}/Group_epi_mean.nii.gz $list
-		3dmask_tool -input $list -frac $thr -prefix ${outDir}/Group_epi_mask.nii.gz
-	fi
+	3dMean -prefix ${outDir}/Group_epi_mean.nii.gz $list
+	3dmask_tool -input $list -frac $thr -prefix ${outDir}/Group_epi_mask.nii.gz
+fi
 
 
-	# make $mask
-	if [ ! -f ${mask}.HEAD ]; then
+# make $mask
+if [ ! -f ${mask}.HEAD ]; then
 
-		# GM mask
-		c3d ${priorDir}/Prior2.nii.gz ${priorDir}/Prior4.nii.gz -add -o tmp_Prior_GM.nii.gz
-		3dresample -master $refFile -rmode NN -input tmp_Prior_GM.nii.gz -prefix tmp_Template_GM_mask.nii.gz
+	# GM mask
+	c3d ${priorDir}/Prior2.nii.gz ${priorDir}/Prior4.nii.gz -add -o tmp_Prior_GM.nii.gz
+	3dresample -master $refFile -rmode NN -input tmp_Prior_GM.nii.gz -prefix tmp_Template_GM_mask.nii.gz
 
-		# combine GM and intersection mask
-		c3d tmp_Template_GM_mask.nii.gz Group_epi_mask.nii.gz -multiply -o tmp_Intersection_GM_prob_mask.nii.gz
-		c3d tmp_Intersection_GM_prob_mask.nii.gz -thresh 0.1 1 1 0 -o tmp_Intersection_GM_mask.nii.gz
-		3dcopy tmp_Intersection_GM_mask.nii.gz $mask
-		rm tmp*
-	fi
+	# combine GM and intersection mask
+	c3d tmp_Template_GM_mask.nii.gz Group_epi_mask.nii.gz -multiply -o tmp_Intersection_GM_prob_mask.nii.gz
+	c3d tmp_Intersection_GM_prob_mask.nii.gz -thresh 0.1 1 1 0 -o tmp_Intersection_GM_mask.nii.gz
+	3dcopy tmp_Intersection_GM_mask.nii.gz $mask
+	rm tmp*
+fi
 
-	if [ ! -f ${mask}.HEAD ]; then
-		echo >&2
-		echo "Could not construct $mask. Exit 5" >&2
-		echo >&2
-		exit 5
-	fi
+if [ ! -f ${mask}.HEAD ]; then
+	echo >&2
+	echo "Could not construct $mask. Exit 5" >&2
+	echo >&2
+	exit 5
+fi
 
 
-	# get template
-	if [ ! -f vold2_mni_brain+tlrc.HEAD ]; then
-		cp ${tempDir}/vold2_mni_brain+tlrc* .
-	fi
+# get template
+if [ ! -f vold2_mni_brain+tlrc.HEAD ]; then
+	cp ${tempDir}/AO_template_brain+tlrc* .
 fi
 
 
 
 
-### --- MVM --- ###
+
+### --- Set up for MVM --- ###
 #
-# This will blur both stats and errts files according to $blurM, and
-# then the blurred errts files will be used to model noise with
-# an auto-correlation function. MVM scripts will be written and run,
-# and none of this will happen on participants who move too much.
-# A variable number of bx/wi subj variables is accepted, but this
-# will not run a t-test.
-#
-# Currently, MVM post-hoc comparisons are permutations of bx/wi-subj
-# variables. I.e. Behaviors A B C for groups Aut Con will yield
-# comparisons of Aut-Con A-B, Aut-Con A-C, Aut-Con B-C. I could build
-# more comparisons in the future.
+# This section will conduct noise simulations
+# to model false positives using the updated version.
+# Simulations will be conducted only on the GM-intersection mask
 
 
 arrCount=0; while [ $arrCount -lt $compLen ]; do
 
 	pref=${compList[$arrCount]}
 	print=ACF_raw_${pref}.txt
-	outPre=${pref}_MVM_REML
+	# outPre=${pref}_MVM_REML
 
 	# make subj list
 	unset subjList
-
 	for j in ${workDir}/s*; do
 
 		arrRem=(`cat info_rmSubj_${pref}.txt`)
 		subj=${j##*\/}
 		MatchString "$subj" "${arrRem[@]}"
-
 		if [ $? == 1 ]; then
 			subjList+=("$subj ")
 		fi
@@ -285,155 +208,36 @@ arrCount=0; while [ $arrCount -lt $compLen ]; do
 	blurH=`echo $gridSize*$blurM | bc`
 	blurInt=`printf "%.0f" $blurH`
 
-	if [ $runIt == 1 ]; then
-		if [ ! -s $print ]; then
-			for k in ${subjList[@]}; do
-				for m in stats errts; do
+	if [ ! -s $print ]; then
+		for k in ${subjList[@]}; do
+			for m in stats errts; do
 
-					hold=${workDir}/${k}/${pref}_${m}_REML
-					file=${workDir}/${k}/${pref}_errts_REML_blur${blurInt}+tlrc
+				hold=${workDir}/${k}/${pref}_${m}_REML
+				file=${workDir}/${k}/${pref}_errts_REML_blur${blurInt}+tlrc
 
-					# blur
-					if [ ! -f ${hold}_blur${blurInt}+tlrc.HEAD ]; then
-						3dmerge -prefix ${hold}_blur${blurInt} -1blur_fwhm $blurInt -doall ${hold}+tlrc
-					fi
-				done
-
-				# parameter estimate
-				3dFWHMx -mask $mask -input $file -acf >> $print
-			done
-		fi
-
-
-		# simulate noise, determine thresholds
-		if [ ! -s ACF_MC_${pref}.txt ]; then
-
-			sed '/ 0  0  0    0/d' $print > tmp
-
-			xA=`awk '{ total += $1 } END { print total/NR }' tmp`
-			xB=`awk '{ total += $2 } END { print total/NR }' tmp`
-			xC=`awk '{ total += $3 } END { print total/NR }' tmp`
-
-			3dClustSim -mask $mask -LOTS -iter 10000 -acf $xA $xB $xC > ACF_MC_${pref}.txt
-			rm tmp
-		fi
-	fi
-
-
-	# set up - determine/construct variables for script
-	scan=${pref}_stats_REML_blur${blurInt}+tlrc
-
-	unset conVar gltCount dataFrame
-
-	if [ ${#bsArr[@]} -gt 1 ]; then
-
-
-		# header, bx-subj title
-		bsVars=BSVARS
-		header="Subj $bsVars WSVARS InputFile"
-
-
-		# make $conVar (post-hoc comparisons)
-		for x in ${!arrBS[@]}; do
-
-			h1=${arrBS[$x]:0:1}
-			h2=${arrBS[$x]:1:1}
-
-			bsCon="1*${bsArr[$h1]} -1*${bsArr[$h2]}"
-			bsLab=${bsArr[$h1]}-${bsArr[$h2]}
-
-			for y in ${!arr[@]}; do
-
-				gltCount=$[$gltCount+1]
-				ws1h=${arr[$y]:0:1}
-				ws2h=${arr[$y]:1:1}
-
-				eval declare -a nam1=(nam${ws1h})
-				eval declare -a nam2=(nam${ws2h})
-				name1=$(eval echo \${${nam1}[$arrCount]})
-				name2=$(eval echo \${${nam2}[$arrCount]})
-
-				conVar+="-gltLabel $gltCount ${bsLab}_${name1}-${name2} -gltCode $gltCount '${bsVars}: $bsCon WSVARS: 1*$name1 -1*$name2' "
-			done
-		done
-
-
-		# determine group membership, write dataframe
-		bsSubj=(`cat $bsList | awk '{print $1}'`)
-		bsGroup=(`cat $bsList | awk '{print $2}'`)
-
-		for m in ${subjList[@]}; do
-			for n in ${!bsSubj[@]}; do
-				if [ $m == ${bsSubj[$n]} ]; then
-					for o in ${wsList[@]}; do
-
-						brik=$(eval echo \${arr${o}[$arrCount]})
-						name=$(eval echo \${nam${o}[$arrCount]})
-
-						dataFrame+="$m ${bsGroup[$n]} $name ${workDir}/${m}/'${scan}[${brik}]' "
-					done
+				# blur
+				if [ ! -f ${hold}_blur${blurInt}+tlrc.HEAD ]; then
+					3dmerge -prefix ${hold}_blur${blurInt} -1blur_fwhm $blurInt -doall ${hold}+tlrc
 				fi
 			done
-		done
 
-	else
-		bsVars=1
-		header="Subj WSVARS InputFile"
-
-		for y in ${!arr[@]}; do
-
-			gltCount=$[$gltCount+1]
-			ws1h=${arr[$y]:0:1}
-			ws2h=${arr[$y]:1:1}
-
-			eval declare -a nam1=(nam${ws1h})
-			eval declare -a nam2=(nam${ws2h})
-			name1=$(eval echo \${${nam1}[$arrCount]})
-			name2=$(eval echo \${${nam2}[$arrCount]})
-
-			conVar+="-gltLabel $gltCount ${name1}-${name2} -gltCode $gltCount 'WSVARS: 1*$name1 -1*$name2' "
-		done
-
-		for m in ${subjList[@]}; do
-			for n in ${wsList[@]}; do
-
-				brik=$(eval echo \${arr${n}[$arrCount]})
-				name=$(eval echo \${nam${n}[$arrCount]})
-
-				dataFrame+="$m $name ${workDir}/${m}/'${scan}[${brik}]' "
-			done
+			# parameter estimate
+			3dFWHMx -mask $mask -input $file -acf >> $print
 		done
 	fi
 
 
-	# write script
-	echo "module load r/3/5
+	# simulate noise, determine thresholds
+	if [ ! -s ACF_MC_${pref}.txt ]; then
 
-		3dMVM -prefix $outPre \\
-		-jobs 10 \\
-		-mask $mask \\
-		-bsVars $bsVars \\
-		-wsVars 'WSVARS' \\
-		-num_glt $gltCount \\
-		$conVar \\
-		-dataTable \\
-		$header \\
-		$dataFrame" > ${outDir}/${outPre}.sh
+		sed '/ 0  0  0    0/d' $print > tmp
 
+		xA=`awk '{ total += $1 } END { print total/NR }' tmp`
+		xB=`awk '{ total += $2 } END { print total/NR }' tmp`
+		xC=`awk '{ total += $3 } END { print total/NR }' tmp`
 
-	# run MVM
-	if [ $runIt == 1 ]; then
-		if [ ! -f ${outPre}+tlrc.HEAD ]; then
-			source ${outDir}/${outPre}.sh
-		fi
-
-		# Check
-		if [ ! -f ${outPre}+tlrc.HEAD ]; then
-			echo >&2
-			echo "MVM failed on $outPre. Exiting. Exit 8" >&2
-			echo >&2
-			exit 8
-		fi
+		3dClustSim -mask $mask -LOTS -iter 10000 -acf $xA $xB $xC > ACF_MC_${pref}.txt
+		rm tmp
 	fi
 
 	let arrCount=$[$arrCount+1]
